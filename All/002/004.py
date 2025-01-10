d96e5eef5176556b4f777df053f1f4b3dfd3f893
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2025-01-10 13:58:55 +0300

    Propagate Color now gets color from preferences and propagates color to multiple selected nodes

--- All/002/004.py
+++ All/002/004.py
@@ -6,19 +6,24 @@
 #
 #----------------------------------------------------------------------------------------------------------
 
+#v2.0 Pushkarev Aleksandr 2025
+#
+#copies first selected node's color to other nodes
+#gets default color(value 0) from preferences
+
 def copyTileColor():
     nodes = nuke.selectedNodes()
-    if len(nodes)<2:
-        return
-    nodes.reverse()
-    color_to_copy = None
-    for node in nodes:
-        color = node['tile_color'].value()
+    if nodes:
+        nodes.reverse()
+        color = nodes[0]['tile_color'].value()
+        if color==0:
+            pref = nuke.toNode('preferences')
+            for knName,kn in pref.knobs().items():
+                if knName.startswith('NodeColourSlot') and nodes[0].Class().lower() in kn.value().split():
+                    color = pref[knName.replace('Slot','Choice')].value()
+                    break
         if color!=0:
-            color_to_copy = color
-            break
-    if color_to_copy!=None:
-        for node in nodes:
-            node['tile_color'].setValue(color_to_copy)
+            for n in nodes[1:]:
+                n['tile_color'].setValue(color)
 
 copyTileColor()
